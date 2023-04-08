5ab60a5a3d19522535aa3ad404d3c90b204fc8a8
dimaslanjaka <dimaslanjaka@gmail.com>
2023-04-08 07:46:57 +0700

    re-init monorepo

---
+++ package/dist/custom-helpers.d.ts
@@ -0,0 +1,7 @@
+import Hexo from 'hexo';
+export declare function getTheAuthor(authorObj: Record<string, any> | string): string;
+/**
+ * register custom helpers
+ * @param hexo
+ */
+export declare function registerCustomHelper(hexo: Hexo): void;
