5ed12bb177fb7a877b314cc92fe177d03de66f99
dimaslanjaka <dimaslanjaka@gmail.com>
2023-10-05 09:18:02 +0700

    feat add render test

--- test/render-markdown.ts
+++ test/render-markdown.ts
@@ -1,10 +1,9 @@
 import renderer from '../src/renderer-markdown-it';
 import Hexo from 'hexo';
+import fs from 'fs'
 
 const hexo = new Hexo(__dirname, { silent: true });
 const r = renderer(hexo);
 
-const code = 'console.log("Hello world");';
-const body = ['# Hello world', '', '```', code, '```', '', '## Hello world', '', 'hello'].join('\n');
-const result = r({ text: body });
+const result = r({ text: fs.readFileSync(__dirname + '/fixtures/sample.md', 'utf-8') });
 console.log(result);
---
+++ test/render-sample.ts
@@ -0,0 +1,20 @@
+import fs from 'fs';
+import Hexo from 'hexo';
+
+const hexo = new Hexo(__dirname, { silent: true });
+const defaultCfg = JSON.parse(JSON.stringify(Object.assign(hexo.config, {
+  marked: {}
+})));
+hexo.config.permalink = ':title';
+hexo.config = JSON.parse(JSON.stringify(defaultCfg));
+hexo.init().then(() => {
+  hexo.post
+    .render(null as any, {
+      content: fs.readFileSync(__dirname + '/fixtures/sample.md', 'utf-8'),
+      // disableNunjucks: true,
+      engine: 'md'
+    })
+    .then(console.log);
+})
+
+
