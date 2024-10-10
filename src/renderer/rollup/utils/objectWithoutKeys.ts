e200f15941850b3d4f8b8d271a8bb8fb5bf0024c
dimaslanjaka <dimaslanjaka@gmail.com>
2024-10-10 08:00:52 +0700

    fix: invalid export

--- src/renderer/rollup/utils/objectWithoutKeys.ts
+++ src/renderer/rollup/utils/objectWithoutKeys.ts
@@ -8,10 +8,13 @@ export const objectWithoutKeys = (obj: Record<string, any>, keys: string[]) => {
   if (!Array.isArray(keys)) {
     throw new TypeError('keys most string[].');
   }
-  return Object.keys(obj).reduce((newObject, key) => {
-    if (!keys.includes(key)) newObject[key] = obj[key];
-    return newObject;
-  }, {} as Record<string, any>);
+  return Object.keys(obj).reduce(
+    (newObject, key) => {
+      if (!keys.includes(key)) newObject[key] = obj[key];
+      return newObject;
+    },
+    {} as Record<string, any>
+  );
 };
 
-module.exports = objectWithoutKeys;
+export default objectWithoutKeys;
