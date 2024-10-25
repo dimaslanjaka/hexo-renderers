074e05934ccbea6dca56b609fd3b5651d59eba35
dimaslanjaka <dimaslanjaka@gmail.com>
2024-10-25 08:39:59 +0700

    skip bundle `markdown-it`

--- rollup.config.js
+++ rollup.config.js
@@ -17,7 +17,7 @@ const __dirname = path.dirname(__filename);
 const pkg = jsonc.parse(fs.readFileSync(path.join(__dirname, 'package.json'), 'utf-8'));
 export const external = Object.keys(pkg.dependencies)
   .concat(Object.keys(pkg.devDependencies))
-  .filter((pkgName) => !['markdown-it', 'p-limit', 'deepmerge-ts'].includes(pkgName));
+  .filter((pkgName) => ![/*'markdown-it', */ 'p-limit', 'deepmerge-ts'].includes(pkgName));
 
 /**
  * @type {import('rollup').RollupOptions}
