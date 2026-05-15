439ef157265f9216f716ae5fc508ec24f756bff0
dimaslanjaka <dimaslanjaka@gmail.com>
2026-05-15 08:29:35 +0700

    fix(mathjax): update import path for HexoLocalsData

--- src/mathjax/index.ts
+++ src/mathjax/index.ts
@@ -1,8 +1,8 @@
 import ejs from 'ejs';
 import fs from 'fs';
-import { HexoLocalsData } from 'hexo/dist/hexo/locals-d';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import { HexoLocalsData } from '../index-exports';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
